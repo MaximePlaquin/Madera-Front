42bcf4fa744e27a8848a06e47c30508baf1089d7
helclaeynn <malekith51@hotmail.com>
2018-03-13 16:38:48 +0100

    tmp

--- src/app/modules/projects/form/form.component.js
+++ src/app/modules/projects/form/form.component.js
@@ -8,14 +8,80 @@
      *
      */
     /* @ngInject */
-    function formProjectsCtrl() {
+    function formProjectsCtrl(projectsService, loggerService, checkInputService) {
         /*jshint validthis : true*/
         var $ctrl = this;
+
+       /**
+         * @ngdoc property
+         * @name app.cors.components.formProjectsCtrl#component
+         * @description objet formulaire composant
+         *
+         */
+        $ctrl.project = {};
+
+        /**
+         * @ngdoc function
+         * @name app.cors.components.formProjectsCtrl#edit
+         * @description appel fonction mise a jour
+         *
+         */
+        $ctrl.send = send;
+
+        /**
+         * @ngdoc function
+         * @name app.cors.components.formProjectsCtrl#send
+         * @description controle creation ou maj
+         *
+         */
+        function send(value){
+            if(checkInputService.isUndefinedOrNull(value)) {
+                if(checkInputService.isUndefinedOrNull(value.id)) {
+                    createProjects(value);
+                } else {
+                    editProjects(value);
+                }
+            }
+        }
+
+        /**
+         * @ngdoc function
+         * @name app.cors.components.formProjectsCtrl#editProjects
+         * @description mise a jour projets
+         *
+         * @param value
+         */
+        function editProjects(value){
+            componentsService.EditProjects(value).then(function success() {
+            }, function error(error) {
+                loggerService.error('projects - edit ',error);
+            });
+        }
+
+        /**
+         * @ngdoc function
+         * @name app.cors.components.formProjectsCtrl#createProjects
+         * @description création projets
+         *
+         * @param value
+         */
+        function createProjects(value){
+            componentsService.CreateProjects(value).then(function success() {
+            }, function error(error) {
+                loggerService.error('projects - create ',error);
+            });
+        }
     }
+
+    formProjectsCtrl.$inject = [ 'projectsService', 'loggerService', 'checkInputService'];
+
 
     angular.module('app.cors.projects')
         .component('formProjects', {
             templateUrl: 'app/modules/projects/form/form.tpl.html',
-            controller: formProjectsCtrl
+            controller: formProjectsCtrl,
+            bindings: {
+                project: '='
+            }
         });
 })(angular);
